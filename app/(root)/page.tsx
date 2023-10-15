19542f7e2ed6ae3a9f9d5554c00a6c000857ca1e
ppalladio <nine.slide@gmail.com>
2023-10-15 19:54:05 +0200

    create store form struc

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,24 @@
 'use client';
 import Modal from '@/components/ui/modal';
+import { useStoreModal } from '@/hooks/useStoreModal';
 import { UserButton } from '@clerk/nextjs';
+import { useEffect } from 'react';
 
 const SetupPage = () => {
+	const onOpen = useStoreModal((state)=>state.onOpen)
+	const isOpen = useStoreModal((state)=>state.isOpen)
+
+	useEffect(() => {
+	  if(!isOpen) {
+		onOpen()
+	  }
+	
+	}, [isOpen, onOpen])
+	
     return (
         <div className="p-4">
-            <Modal title="test" description="test" isOpen onClose={() => {}}>
-                children
-            </Modal>
+         
+		 root page
         </div>
     );
 };
--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { StoreModal } from '@/components/modals/storeModal';
+import { StoreModal } from '@/components/modals/StoreModal';
 import { useEffect, useState } from 'react';
 
 const ModalProvider = () => {
