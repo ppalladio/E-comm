61f5581691a0824a6f0c5861991bcf29d7db3615
ppalladio <nine.slide@gmail.com>
2023-10-20 19:36:31 +0200

    admin navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,30 @@
-import { UserButton } from '@clerk/nextjs';
+import { UserButton, auth } from '@clerk/nextjs';
 import MainNav from '@/components/MainNav';
+import StoreSwitcher from '@/components/StoreSwitcher';
+import { redirect } from 'next/navigation';
+import prismadb from '@/lib/prismadb';
 
-const Navbar = () => {
+const Navbar = async () => {
+    const { userId } = auth();
+
+    if (!userId) {
+        redirect('/sign-in');
+    }
+
+    const stores = await prismadb.store.findMany({
+        where: {
+            userId,
+        },
+    });
     return (
         <div className="boarder-b">
             <div className="flex h-16 items-center px-4">
-                <div>store switcher // </div>
-                <MainNav className='mx-6'/>
+                <StoreSwitcher items={stores} />
+                <MainNav className="mx-6" />
                 <div className="ml-auto flex items-center space-x-4">
-                    <UserButton afterSignOutUrl='/' />
-                NM</div>
+                    <UserButton afterSignOutUrl="/" />
+                    NM
+                </div>
             </div>
         </div>
     );
