cc02e55ec287b09e250207075d161d8a39d464a4
ppalladio <nine.slide@gmail.com>
2024-02-10 04:12:13 +0100

    deploy fix

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { StoreModal } from '@/components/modals/StoreModal';
+import { StoreModal } from '@/components/modals/storeModal';
 import { useEffect, useState } from 'react';
 
 const ModalProvider = () => {
