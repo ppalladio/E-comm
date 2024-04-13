a41489981f1f5306667f212f55eba0ca896afd5f
ppalladio <nine.slide@gmail.com>
2024-04-13 18:47:10 +0200

    minor

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,14 +2,16 @@ import prismadb from '@/lib/prismadb';
 import { auth } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 
-export default async function SetupLayout({children}: {
-    children: React.ReactNode;
+export default async function SetupLayout({
+    children,
+}: {
+    readonly children: React.ReactNode;
 }) {
     const { userId } = auth();
     if (!userId) {
         redirect('/sign-in');
     }
-
+    console.log(process.env.DATABASE_URL);
     const store = await prismadb.store.findFirst({
         where: {
             userId,
